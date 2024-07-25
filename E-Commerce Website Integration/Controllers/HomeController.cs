fa5130ba86eaa3df4c1ef8b301eb4ec4b771bdfa
Sarfaraz Qadir <sarfarazqadir594@gmail.com.com>
2024-07-25 11:24:04 +0500

    Add project files.

---
+++ E-Commerce Website Integration/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce_Website_Integration.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
